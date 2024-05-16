52e3a4738e52a65acd80572273aa4dd3d7799c1a
birdie <flaehdan@gmail.com>
2024-05-16 09:22:46 +0900

    보기 수정

--- src/main/java/org/example/section2/decorator/_02_after/Application.java
+++ src/main/java/org/example/section2/decorator/_02_after/Application.java
@@ -17,8 +17,8 @@ public class Application {
         }
 
         Client client = new Client(commentService);
-        client.writeComment("오징어게임");
-        client.writeComment("보는게 하는거 보다 재밌을 수가 없지...");
+        client.writeComment("write comment");
+        client.writeComment("...제거 테스트...");
         client.writeComment("http://donghyun.me");
     }
 }
